01e97f7f295a33cbe2cc8f208233c0e7de5a43fa
tytu16 <ttuschhoff11@gmail.com>
2022-07-29 09:36:27 -0500

    Changing param name constants

--- Source/synth/ParamsList.h
+++ Source/synth/ParamsList.h
@@ -1,62 +1,55 @@
 #pragma once
 
 // String constants pointing to their respective parameter paths in Faust
-const char * const PANIC = "Panic";
+const char * const PANIC       = "Panic";
 
-// OSC1 PARAMS
-const char * const OSC1_RANGE = "/faug/faug/osc_bank/osc-1/range";
-const char * const OSC1_FREQ = "/faug/faug/osc_bank/osc-1/freq";
-const char * const OSC1_DETUNE = "/faug/faug/osc_bank/osc-1/detune";
-const char * const OSC1_DRIFT = "/faug/faug/osc_bank/osc-1/drift_amount";
-const char * const OSC1_DRATE = "/faug/faug/osc_bank/osc-1/drift_rate";
-const char * const OSC1_WAVE = "/faug/faug/osc_bank/osc-1/wave";
-
-// OSC2 PARAMS
-const char * const OSC2_RANGE = "/faug/faug/osc_bank/osc-2/range";
-const char * const OSC2_FREQ = "/faug/faug/osc_bank/osc-2/freq";
-const char * const OSC2_DETUNE = "/faug/faug/osc_bank/osc-2/detune";
-const char * const OSC2_DRIFT = "/faug/faug/osc_bank/osc-2/drift_amount";
-const char * const OSC2_DRATE = "/faug/faug/osc_bank/osc-2/drift_rate";
-const char * const OSC2_WAVE = "/faug/faug/osc_bank/osc-2/wave";
-
-// OSC3 PARAMS
-const char * const OSC3_RANGE = "/faug/faug/osc_bank/osc-3/range";
-const char * const OSC3_FREQ = "/faug/faug/osc_bank/osc-3/freq";
-const char * const OSC3_DETUNE = "/faug/faug/osc_bank/osc-3/detune";
-const char * const OSC3_DRIFT = "/faug/faug/osc_bank/osc-3/drift_amount";
-const char * const OSC3_DRATE = "/faug/faug/osc_bank/osc-3/drift_rate";
-const char * const OSC3_WAVE = "/faug/faug/osc_bank/osc-3/wave";
-
-// ENVELOPE PARAMS
-const char * const GAIN = "/faug/gain";
-const char * const ATTACK = "/faug/faug/envelope/attack";
-const char * const DECAY = "/faug/faug/envelope/decay";
-const char * const SUSTAIN = "/faug/faug/envelope/sustain";
-const char * const GATE = "/faug/faug/envelope/gate";
-
-// FILTER PARAMS
-const char * const F_CUTOFF = "/faug/faug/filter/cutoff";
-const char * const F_Q = "/faug/faug/filter/emphasis";
-const char * const F_ATTACK = "/faug/faug/filter/filter_contour/attack";
-const char * const F_ENV_AMOUNT = "/faug/faug/filter/filter_contour/amount";
-const char * const F_DECAY = "/faug/faug/filter/filter_contour/decay";
-const char * const F_SUSTAIN = "/faug/faug/filter/filter_contour/sustain";
-const char * const F_ENV_GATE = "/faug/faug/filter/filter_contour/gate";
+// FREQ
+const char* const FREQ         = "freq";
 
 // FEEDBACK PARAMS
-const char * const FEEDBACK1 = "/faug/faug/feedback";
-const char * const FEEDBACK2 = "/faug/feedback";
+const char* const FEEDBACK     = "feedback";
 
-const char * const PARAMS_LIST[] = {
-	OSC1_RANGE, OSC1_FREQ, OSC1_DETUNE, OSC1_DRIFT, OSC1_DRATE, OSC1_WAVE,
-	OSC2_RANGE, OSC2_FREQ, OSC2_DETUNE, OSC2_DRIFT, OSC2_DRATE, OSC2_WAVE,
-	OSC3_RANGE, OSC3_FREQ, OSC3_DETUNE, OSC3_DRIFT, OSC3_DRATE, OSC3_WAVE,
-	GAIN, ATTACK, DECAY, SUSTAIN, GATE, 
-	F_CUTOFF, F_Q, F_ATTACK, F_ENV_AMOUNT, F_DECAY, F_SUSTAIN, F_ENV_GATE,
-	FEEDBACK1, FEEDBACK2
+// OSC1 PARAMS
+const char * const OSC1_RANGE  = "rangeOne";
+const char * const OSC1_DETUNE = "detuneOne";
+const char * const OSC1_WAVE   = "waveOne";
+
+// OSC2 PARAMS
+const char * const OSC2_RANGE  = "rangeTwo";
+const char * const OSC2_DETUNE = "detuneTwo";
+const char * const OSC2_WAVE   = "waveTwo";
+
+// OSC3 PARAMS
+const char * const OSC3_RANGE  = "rangeThree";
+const char * const OSC3_DETUNE = "detuneThree";
+const char * const OSC3_WAVE   = "waveThree";
+
+// ENVELOPE PARAMS
+const char * const ATTACK      = "eAttack";
+const char * const DECAY       = "eDecay";
+const char * const SUSTAIN     = "eSustain";
+const char * const GATE        = "gate";
+//const char* const F_GATE       = "gate";
+
+
+// FILTER PARAMS
+const char * const F_CUTOFF    = "cutoff";
+const char * const F_Q         = "emphasis";
+const char * const F_ENV_AMOUNT= "contourAmount";
+const char * const F_ATTACK    = "fAttack";
+const char * const F_DECAY     = "fDecay";
+const char * const F_SUSTAIN   = "fSustain";
+
+const char * const PARAMS_LIST[] = { 
+	OSC1_RANGE, OSC1_DETUNE, OSC1_WAVE,
+	OSC2_RANGE, OSC2_DETUNE, OSC2_WAVE,
+	OSC3_RANGE, OSC3_DETUNE, OSC3_WAVE,
+	ATTACK, DECAY, SUSTAIN, GATE, FREQ,
+	F_CUTOFF, F_Q, F_ATTACK, F_ENV_AMOUNT, F_DECAY, F_SUSTAIN,
+	FEEDBACK
 };
 
 const char * const KEY_PARAMS[] = {
-	GAIN, GATE, OSC1_FREQ
+	GATE, FREQ
 };
-const int NUM_KEY = 3;
+const int NUM_KEY = 2;
