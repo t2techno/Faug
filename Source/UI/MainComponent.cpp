ac7909b69b757a919fc31a2128cbe9a63265db38
tytu16 <ttuschhoff11@gmail.com>
2022-08-09 22:24:49 -0500

    Using new knob types, beginning process of making screen scalable

--- Source/UI/MainComponent.cpp
+++ Source/UI/MainComponent.cpp
@@ -2,17 +2,10 @@
 // Todo: Move MidiKeyboardComponent to it's own class to be added/removed
 
 #include "MainComponent.h"
-#include "Knob.h"
 #include "../ParamsList.h"
 
-const int MainComponent::UPL_X = 215;
-const int MainComponent::UPL_Y = 65;
-const int MainComponent::COL_W = 100;
-const int MainComponent::ROW_H = 100;
-const int MainComponent::COMP_SIZE = 80;
-
-MainComponent::MainComponent(juce::MidiKeyboardState& keyboardState, juce::AudioProcessorValueTreeState& vts)
-    : m_vts(vts),
+MainComponent::MainComponent(juce::MidiKeyboardState& keyboardState, juce::AudioProcessorValueTreeState& vts, juce::Rectangle<int> screen)
+    : m_vts(vts), screenWidth(screen.getWidth()), screenHeight(screen.getHeight()),
     keyboardComponent(keyboardState, juce::MidiKeyboardComponent::horizontalKeyboard)
 
 {
@@ -22,160 +15,171 @@ MainComponent::MainComponent(juce::MidiKeyboardState& keyboardState, juce::Audio
     addAndMakeVisible(keyboardComponent);
     keyboardComponent.setKeyWidth(40);
 
-    createOscBank();
+    int oscBank_x = screenWidth * 0.1825;
+    int oscBank_y = screenHeight * 0.175;
+    int oscBankCol_w = screenWidth * 0.085;
+    int oscBankRow_h = screenHeight * 0.138;
 
-    createFilterBank();
+    knob_size = screenHeight * 0.095;
+    small_knob_size = screenHeight * 0.08;
+    
+    createOscBank(oscBank_x, oscBank_y, oscBankCol_w, oscBankRow_h);
 
-    // ENVELOPE
-    /*m_decaySlider = std::make_unique<juce::Slider>();
-    m_decayLabel = std::make_unique<juce::Label>();
-    createSliderAndLabel(m_decayLabel.get(), "Decay", m_decaySlider.get(),
-        970, 265, SLIDER_SIZE, SLIDER_SIZE);
-    m_decayAttach.reset(new SliderAttachment(m_vts, juce::String(DECAY), *m_decaySlider.get()));*/
+    int mixer_x = screenWidth * 0.45;
+    int mixer_y = screenHeight * 0.175;
+    int mixerCol_w = screenWidth * 0.085;
+    int mixerRow_h = oscBankRow_h/2;
+    createMixer(mixer_x, mixer_y, mixerCol_w, mixerRow_h);
 
+    int filter_x = screenWidth * 0.675;
+    int filter_y = screenHeight * 0.175;
+    int filterCol_w = screenWidth * 0.085;
+    int filterRow_h = oscBankRow_h;
+    createFilterBank(filter_x, filter_y, filterCol_w, filterRow_h);
+
+    int envelope_x = screenWidth * 0.675;
+    int envelope_y = mixer_y + 2* oscBankRow_h;
+    int envelopeCol_w = screenWidth * 0.085;
+    createEnvelope(envelope_x, envelope_y, envelopeCol_w);
 
     // wait .4 seconds, then grab keyboard focus to use as potential midi-input
-    setSize(WIDTH, HEIGHT);
+    setSize(screenWidth, screenHeight);
     startTimer(400);
 }
 
 MainComponent::~MainComponent()
 {}
 
-void MainComponent::createButtonAndLabel(juce::Label* label, std::string labelText, juce::Button* button,
-    int x, int y, int width, int height) 
-{
-    addAndMakeVisible(button);
-    button->setBounds(x, y, width, height);
-    if (label == NULL) {
-        return;
-    }
-    addAndMakeVisible(label);
-    label->setFont(juce::Font(15.00f, juce::Font::plain));
-    label->setText(labelText, juce::dontSendNotification);
-    label->setJustificationType(juce::Justification::centred);
-    label->setEditable(false, true, false);
-    label->setColour(juce::Label::textColourId, juce::Colour(0xffff8917));
-    label->attachToComponent(button, false);
-}
-
-void MainComponent::createOscBank()
+void MainComponent::createOscBank(int upl_x, int upl_y, int col_w, int row_h)
 {
     // OSC ONE
     // col 1
-    m_oscOneRange = std::make_unique<Knob>(m_vts, "Osc1 Range", juce::String(OSC1_RANGE), COMP_SIZE);
-    m_oscOneRange->setBounds(UPL_X, UPL_Y, COMP_SIZE, COMP_SIZE);
+    m_oscOneRange = std::make_unique<KnobTwo>(m_vts, "Osc1 Range", juce::String(OSC1_RANGE), knob_size);
+    m_oscOneRange->setBounds(upl_x, upl_y, knob_size, knob_size);
     addAndMakeVisible(m_oscOneRange.get());
 
     // col 3
-    m_oscOneWaveForm = std::make_unique<Knob>(m_vts, "Osc1 Wave", juce::String(OSC1_WAVE), COMP_SIZE);
-    m_oscOneWaveForm->setBounds(UPL_X + 2 * COL_W, UPL_Y, COMP_SIZE, COMP_SIZE);
+    m_oscOneWaveForm = std::make_unique<KnobTwo>(m_vts, "Osc1 Wave", juce::String(OSC1_WAVE), knob_size);
+    m_oscOneWaveForm->setBounds(upl_x + 2 * col_w, upl_y, knob_size, knob_size);
     addAndMakeVisible(m_oscOneWaveForm.get());
-
-    // col 4
-    m_oscOneGain = std::make_unique<Knob>(m_vts, "Osc1 Gain", juce::String(OSC1_GAIN), COMP_SIZE);
-    m_oscOneGain->setBounds(UPL_X + 3*COL_W, UPL_Y, COMP_SIZE, COMP_SIZE);
-    addAndMakeVisible(m_oscOneGain.get());
-
-    // col 5
-    m_oscOnePowerButton = std::make_unique<juce::ToggleButton>("Osc1");
-    createButtonAndLabel(NULL, "Osc1", m_oscOnePowerButton.get(),
-        UPL_X+4*COL_W, UPL_Y, COMP_SIZE, COMP_SIZE);
-    m_oscOnePowerAttach.reset(new ButtonAttachment(m_vts, juce::String(OSC1_POWER), *m_oscOnePowerButton.get()));
-
     
     // OSC Two
     // col 1
-    m_oscTwoRange = std::make_unique<Knob>(m_vts, "Osc2 Range", juce::String(OSC2_RANGE), COMP_SIZE);
-    m_oscTwoRange->setBounds(UPL_X, UPL_Y+ROW_H, COMP_SIZE, COMP_SIZE);
+    m_oscTwoRange = std::make_unique<KnobTwo>(m_vts, "Osc2 Range", juce::String(OSC2_RANGE), knob_size);
+    m_oscTwoRange->setBounds(upl_x, upl_y + row_h, knob_size, knob_size);
     addAndMakeVisible(m_oscTwoRange.get());
 
     //col 2
-    m_oscTwoDetune = std::make_unique<Knob>(m_vts, "Osc2 Detune", juce::String(OSC2_DETUNE), COMP_SIZE);
-    m_oscTwoDetune->setBounds(UPL_X + COL_W, UPL_Y+ROW_H, COMP_SIZE, COMP_SIZE);
+    m_oscTwoDetune = std::make_unique<KnobOne>(m_vts, "Osc2 Detune", juce::String(OSC2_DETUNE), knob_size);
+    m_oscTwoDetune->setBounds(upl_x + col_w, upl_y + row_h, knob_size, knob_size);
     addAndMakeVisible(m_oscTwoDetune.get());
 
     // col 3
-    m_oscTwoWaveForm = std::make_unique<Knob>(m_vts, "Osc2 Wave", juce::String(OSC2_WAVE), COMP_SIZE);
-    m_oscTwoWaveForm->setBounds(UPL_X + 2 * COL_W, UPL_Y+ROW_H, COMP_SIZE, COMP_SIZE);
+    m_oscTwoWaveForm = std::make_unique<KnobTwo>(m_vts, "Osc2 Wave", juce::String(OSC2_WAVE), knob_size);
+    m_oscTwoWaveForm->setBounds(upl_x + 2 * col_w, upl_y + row_h, knob_size, knob_size);
     addAndMakeVisible(m_oscTwoWaveForm.get());
-
-    // col 4
-    m_oscTwoGain = std::make_unique<Knob>(m_vts, "Osc2 Gain", juce::String(OSC2_GAIN), COMP_SIZE);
-    m_oscTwoGain->setBounds(UPL_X + 3 * COL_W, UPL_Y+ROW_H, COMP_SIZE, COMP_SIZE);
-    addAndMakeVisible(m_oscTwoGain.get());
-
-    // col 5
-    m_oscTwoPowerButton = std::make_unique<juce::ToggleButton>("Osc2");
-    createButtonAndLabel(NULL, "Osc2", m_oscTwoPowerButton.get(),
-        UPL_X + 4 * COL_W, UPL_Y+ROW_H, COMP_SIZE, COMP_SIZE);
-    m_oscTwoPowerAttach.reset(new ButtonAttachment(m_vts, juce::String(OSC2_POWER), *m_oscTwoPowerButton.get()));
-
 
     // OSC Three
     // col 1
-    m_oscThreeRange = std::make_unique<Knob>(m_vts, "Osc3 Range", juce::String(OSC3_RANGE), COMP_SIZE);
-    m_oscThreeRange->setBounds(UPL_X, UPL_Y + 2*ROW_H, COMP_SIZE, COMP_SIZE);
+    m_oscThreeRange = std::make_unique<KnobTwo>(m_vts, "Osc3 Range", juce::String(OSC3_RANGE), knob_size);
+    m_oscThreeRange->setBounds(upl_x, upl_y + 2* row_h, knob_size, knob_size);
     addAndMakeVisible(m_oscThreeRange.get());
 
     //col 2
-    m_oscThreeDetune = std::make_unique<Knob>(m_vts, "Osc3 Detune", juce::String(OSC3_DETUNE), COMP_SIZE);
-    m_oscThreeDetune->setBounds(UPL_X + COL_W, UPL_Y + 2*ROW_H, COMP_SIZE, COMP_SIZE);
+    m_oscThreeDetune = std::make_unique<KnobOne>(m_vts, "Osc3 Detune", juce::String(OSC3_DETUNE), knob_size);
+    m_oscThreeDetune->setBounds(upl_x + col_w, upl_y + 2* row_h, knob_size, knob_size);
     addAndMakeVisible(m_oscThreeDetune.get());
 
     // col 3
-    m_oscThreeWaveForm = std::make_unique<Knob>(m_vts, "Osc3 Wave", juce::String(OSC3_WAVE), COMP_SIZE);
-    m_oscThreeWaveForm->setBounds(UPL_X + 2*COL_W, UPL_Y + 2*ROW_H, COMP_SIZE, COMP_SIZE);
+    m_oscThreeWaveForm = std::make_unique<KnobTwo>(m_vts, "Osc3 Wave", juce::String(OSC3_WAVE), knob_size);
+    m_oscThreeWaveForm->setBounds(upl_x + 2* col_w, upl_y + 2* row_h, knob_size, knob_size);
     addAndMakeVisible(m_oscThreeWaveForm.get());
+}
 
-    // col 4
-    m_oscThreeGain = std::make_unique<Knob>(m_vts, "Osc3 Gain", juce::String(OSC3_GAIN), COMP_SIZE);
-    m_oscThreeGain->setBounds(UPL_X + 3*COL_W, UPL_Y+ 2*ROW_H, COMP_SIZE, COMP_SIZE);
+void MainComponent::createMixer(int upl_x, int upl_y, int col_w, int row_h)
+{
+    // row 1
+    m_oscOneGain = std::make_unique<KnobThree>(m_vts, "Osc1 Gain", juce::String(OSC1_GAIN), small_knob_size);
+    m_oscOneGain->setBounds(upl_x, upl_y, small_knob_size, small_knob_size);
+    addAndMakeVisible(m_oscOneGain.get());
+
+    m_oscOnePowerButton = std::make_unique<juce::ToggleButton>("Osc1");
+    addAndMakeVisible(m_oscOnePowerButton.get());
+    m_oscOnePowerButton->setBounds(upl_x + col_w, upl_y, knob_size, knob_size);
+    m_oscOnePowerAttach.reset(new ButtonAttachment(m_vts, juce::String(OSC1_POWER), *m_oscOnePowerButton.get()));
+
+    // row 3
+    m_oscTwoGain = std::make_unique<KnobThree>(m_vts, "Osc2 Gain", juce::String(OSC2_GAIN), small_knob_size);
+    m_oscTwoGain->setBounds(upl_x, upl_y + 2*row_h, small_knob_size, small_knob_size);
+    addAndMakeVisible(m_oscTwoGain.get());
+
+    m_oscTwoPowerButton = std::make_unique<juce::ToggleButton>("Osc2");
+    addAndMakeVisible(m_oscTwoPowerButton.get());
+    m_oscTwoPowerButton->setBounds(upl_x + col_w, upl_y + 2 * row_h, knob_size, knob_size);
+    m_oscTwoPowerAttach.reset(new ButtonAttachment(m_vts, juce::String(OSC2_POWER), *m_oscTwoPowerButton.get()));
+
+    // row 5
+    m_oscThreeGain = std::make_unique<KnobThree>(m_vts, "Osc3 Gain", juce::String(OSC3_GAIN), small_knob_size);
+    m_oscThreeGain->setBounds(upl_x, upl_y + 4 * row_h, small_knob_size, small_knob_size);
     addAndMakeVisible(m_oscThreeGain.get());
 
-    // col 5
     m_oscThreePowerButton = std::make_unique<juce::ToggleButton>("Osc3");
-    createButtonAndLabel(NULL, "Osc3", m_oscThreePowerButton.get(),
-        UPL_X + 4 * COL_W, UPL_Y + 2*ROW_H, COMP_SIZE, COMP_SIZE);
+    addAndMakeVisible(m_oscThreePowerButton.get());
+    m_oscThreePowerButton->setBounds(upl_x + col_w, upl_y + 4 * row_h, knob_size, knob_size);
     m_oscThreePowerAttach.reset(new ButtonAttachment(m_vts, juce::String(OSC3_POWER), *m_oscThreePowerButton.get()));
 }
 
-void MainComponent::createFilterBank()
+void MainComponent::createFilterBank(int upl_x, int upl_y, int col_w, int row_h)
 {
-
-    int F_UP_LEFT_X = UPL_X + 6.5 * COL_W;
     // ROW ONE - FREQUENCY STUFF
     // COL ONE
-    m_filterCutoff = std::make_unique<Knob>(m_vts, "Cutoff", juce::String(F_CUTOFF), COMP_SIZE);
-    m_filterCutoff->setBounds(F_UP_LEFT_X, UPL_Y, COMP_SIZE, COMP_SIZE);
+    m_filterCutoff = std::make_unique<KnobThree>(m_vts, "Cutoff", juce::String(F_CUTOFF), small_knob_size);
+    m_filterCutoff->setBounds(upl_x, upl_y, small_knob_size, small_knob_size);
     addAndMakeVisible(m_filterCutoff.get());
 
     // COL TWO
-    m_filterQ = std::make_unique<Knob>(m_vts, "Emphasis", juce::String(F_Q), COMP_SIZE);
-    m_filterQ->setBounds(F_UP_LEFT_X + COL_W, UPL_Y, COMP_SIZE, COMP_SIZE);
+    m_filterQ = std::make_unique<KnobThree>(m_vts, "Emphasis", juce::String(F_Q), small_knob_size);
+    m_filterQ->setBounds(upl_x + col_w, upl_y, small_knob_size, small_knob_size);
     addAndMakeVisible(m_filterQ.get());
 
     // COL THREE
-    m_filterContourAmount = std::make_unique<Knob>(m_vts, "Contour Amount", juce::String(F_ENV_AMOUNT), COMP_SIZE);
-    m_filterContourAmount->setBounds(F_UP_LEFT_X + 2*COL_W, UPL_Y, COMP_SIZE, COMP_SIZE);
+    m_filterContourAmount = std::make_unique<KnobThree>(m_vts, "Contour Amount", juce::String(F_ENV_AMOUNT), small_knob_size);
+    m_filterContourAmount->setBounds(upl_x + 2* col_w, upl_y, small_knob_size, small_knob_size);
     addAndMakeVisible(m_filterContourAmount.get());
 
 
     // ROW TWO - FILTER ENVELOPE STUFF
     // COL ONE
-    m_filterAttack = std::make_unique<Knob>(m_vts, "Attack", juce::String(F_ATTACK), COMP_SIZE);
-    m_filterAttack->setBounds(F_UP_LEFT_X, UPL_Y + ROW_H, COMP_SIZE, COMP_SIZE);
+    m_filterAttack = std::make_unique<KnobThree>(m_vts, "Attack", juce::String(F_ATTACK), small_knob_size);
+    m_filterAttack->setBounds(upl_x, upl_y + row_h, small_knob_size, small_knob_size);
     addAndMakeVisible(m_filterAttack.get());
 
     // COL TWO
-    m_filterDecay = std::make_unique<Knob>(m_vts, "Decay", juce::String(F_DECAY), COMP_SIZE);
-    m_filterDecay->setBounds(F_UP_LEFT_X +COL_W, UPL_Y + ROW_H, COMP_SIZE, COMP_SIZE);
+    m_filterDecay = std::make_unique<KnobThree>(m_vts, "Decay", juce::String(F_DECAY), small_knob_size);
+    m_filterDecay->setBounds(upl_x + col_w, upl_y + row_h, small_knob_size, small_knob_size);
     addAndMakeVisible(m_filterDecay.get());
 
     // COL THREE
-    m_filterSustain = std::make_unique<Knob>(m_vts, "Sustain", juce::String(F_SUSTAIN), COMP_SIZE);
-    m_filterSustain->setBounds(F_UP_LEFT_X + 2*COL_W, UPL_Y + ROW_H, COMP_SIZE, COMP_SIZE);
+    m_filterSustain = std::make_unique<KnobThree>(m_vts, "Sustain", juce::String(F_SUSTAIN), small_knob_size);
+    m_filterSustain->setBounds(upl_x + 2* col_w, upl_y + row_h, small_knob_size, small_knob_size);
     addAndMakeVisible(m_filterSustain.get());
+}
+
+void MainComponent::createEnvelope(int upl_x, int upl_y, int col_w)
+{
+    m_attack = std::make_unique<KnobThree>(m_vts, "Attack", juce::String(ATTACK), small_knob_size);
+    m_attack->setBounds(upl_x, upl_y, small_knob_size, small_knob_size);
+    addAndMakeVisible(m_attack.get());
+
+    // COL TWO
+    m_decay = std::make_unique<KnobThree>(m_vts, "Decay", juce::String(DECAY), small_knob_size);
+    m_decay->setBounds(upl_x + col_w, upl_y, small_knob_size, small_knob_size);
+    addAndMakeVisible(m_decay.get());
+
+    // COL THREE
+    m_sustain = std::make_unique<KnobThree>(m_vts, "Sustain", juce::String(SUSTAIN), small_knob_size);
+    m_sustain->setBounds(upl_x + 2*col_w, upl_y, small_knob_size, small_knob_size);
+    addAndMakeVisible(m_sustain.get());
 }
 
 void MainComponent::paint(juce::Graphics& g)
@@ -184,14 +188,14 @@ void MainComponent::paint(juce::Graphics& g)
     jassert(background != 0);
     if (background != 0)
     {
-        background->drawWithin(g, juce::Rectangle<float>(0, 0, WIDTH, HEIGHT),
+        background->drawWithin(g, juce::Rectangle<float>(0, 0, screenWidth, screenHeight),
             juce::RectanglePlacement::stretchToFit, 1.000f);
     }
 
 }
 
 void MainComponent::resized()
-{    auto area = getLocalBounds().removeFromBottom(200).removeFromRight(WIDTH-40).removeFromLeft(WIDTH - 550);
+{    auto area = getLocalBounds().removeFromBottom(200).removeFromRight(screenWidth-40).removeFromLeft(screenWidth - 550);
     keyboardComponent.setBounds(area);
 }
 
--- Source/UI/MainComponent.h
+++ Source/UI/MainComponent.h
@@ -3,17 +3,16 @@
 // Main Component, holds the primary synth UI
 
 #include <JuceHeader.h>
+#include "./Knobs/Knob.h"
 
 typedef juce::AudioProcessorValueTreeState::ButtonAttachment ButtonAttachment;
 typedef juce::AudioProcessorValueTreeState::SliderAttachment SliderAttachment;
-
-class Knob;
 
 class MainComponent : public juce::Component,
 							 juce::Timer
 {
 public:
-	MainComponent(juce::MidiKeyboardState& keyboardState, juce::AudioProcessorValueTreeState& vts);
+	MainComponent(juce::MidiKeyboardState& keyboardState, juce::AudioProcessorValueTreeState& vts, juce::Rectangle<int> screen);
 	virtual ~MainComponent() override;
 
 	 void  timerCallback() override; // timer
@@ -21,23 +20,19 @@ public:
 	 void paint(juce::Graphics& g) override; //Component
 	 void resized() override; //Component
 
-	 void createButtonAndLabel(juce::Label* label, std::string labelText, juce::Button* slider,
-		 int x, int y, int width, int height);
-
 	 // Create UI Sections
-	 void createOscBank();
+	 void createOscBank(int upl_x, int upl_y, int col_w, int row_h);
 
-	 void createFilterBank();
+	 void createMixer(int upl_x, int upl_y, int col_w, int row_h);
 
+	 void createFilterBank(int upl_x, int upl_y, int col_w, int row_h);
+
+	 void createEnvelope(int upl_x, int upl_y, int col_w);
 private:
-
-	// Constants
-	static const int UPL_X;
-	static const int UPL_Y;
-	static const int COL_W;
-	static const int ROW_H;
-	static const int SECT_W;
-	static const int COMP_SIZE;
+	int screenWidth;
+	int screenHeight;
+	int knob_size;
+	int small_knob_size;
 
 	juce::MidiKeyboardComponent keyboardComponent;
     juce::AudioProcessorValueTreeState& m_vts;
@@ -48,38 +43,40 @@ private:
 	std::unique_ptr<juce::ToggleButton> m_oscOnePowerButton;
 	std::unique_ptr<ButtonAttachment>   m_oscOnePowerAttach;
 
-	std::unique_ptr<Knob> m_oscOneGain;
-	std::unique_ptr<Knob> m_oscOneRange;
-	std::unique_ptr<Knob> m_oscOneWaveForm;
+	std::unique_ptr<KnobThree> m_oscOneGain;
+	std::unique_ptr<KnobTwo> m_oscOneRange;
+	std::unique_ptr<KnobTwo> m_oscOneWaveForm;
 
 	// OSC TWO
 	std::unique_ptr<juce::ToggleButton> m_oscTwoPowerButton;
 	std::unique_ptr<ButtonAttachment>   m_oscTwoPowerAttach;
 
-	std::unique_ptr<Knob> m_oscTwoGain;
-	std::unique_ptr<Knob> m_oscTwoRange;
-	std::unique_ptr<Knob> m_oscTwoWaveForm;
-	std::unique_ptr<Knob> m_oscTwoDetune;
+	std::unique_ptr<KnobThree> m_oscTwoGain;
+	std::unique_ptr<KnobTwo> m_oscTwoRange;
+	std::unique_ptr<KnobTwo> m_oscTwoWaveForm;
+	std::unique_ptr<KnobOne> m_oscTwoDetune;
 
 	// OSC THREE
 	std::unique_ptr<juce::ToggleButton> m_oscThreePowerButton;
 	std::unique_ptr<ButtonAttachment>   m_oscThreePowerAttach;
 
-	std::unique_ptr<Knob> m_oscThreeGain;
-	std::unique_ptr<Knob> m_oscThreeRange;
-	std::unique_ptr<Knob> m_oscThreeWaveForm;
-	std::unique_ptr<Knob> m_oscThreeDetune;
+	std::unique_ptr<KnobThree> m_oscThreeGain;
+	std::unique_ptr<KnobTwo> m_oscThreeRange;
+	std::unique_ptr<KnobTwo> m_oscThreeWaveForm;
+	std::unique_ptr<KnobOne> m_oscThreeDetune;
 
 	// FILTER
-	std::unique_ptr<Knob> m_filterCutoff;
-	std::unique_ptr<Knob> m_filterQ;
-	std::unique_ptr<Knob> m_filterContourAmount;
-	std::unique_ptr<Knob> m_filterAttack;
-	std::unique_ptr<Knob> m_filterDecay;
-	std::unique_ptr<Knob> m_filterSustain;
+	std::unique_ptr<KnobThree> m_filterCutoff;
+	std::unique_ptr<KnobThree> m_filterQ;
+	std::unique_ptr<KnobThree> m_filterContourAmount;
+	std::unique_ptr<KnobThree> m_filterAttack;
+	std::unique_ptr<KnobThree> m_filterDecay;
+	std::unique_ptr<KnobThree> m_filterSustain;
 
 	// ENVELOPE
-	std::unique_ptr<Knob> m_Decay;
+	std::unique_ptr<KnobThree> m_attack;
+	std::unique_ptr<KnobThree> m_decay;
+	std::unique_ptr<KnobThree> m_sustain;
 
 	// GLOBAL
 	std::unique_ptr<juce::Slider>       m_globalDetuneSlider;
