6fc534190f161ea662484ea4dbca8d3b63efe560
tytu16 <ttuschhoff11@gmail.com>
2022-08-09 22:24:10 -0500

    Contains a KnobImage  to draw

---
+++ Source/UI/Knobs/KnobLookAndFeel.cpp
@@ -0,0 +1,112 @@
+/*
+  ==============================================================================
+
+    FaugLookAndFeel.cpp
+    Created: 8 Aug 2022 11:55:23am
+    Author:  tytu1
+
+  ==============================================================================
+*/
+
+#include <JuceHeader.h>
+#include "KnobLookAndFeel.h"
+#include "KnobImage.h"
+//==============================================================================
+KnobLookAndFeel::KnobLookAndFeel()
+{
+}
+
+KnobLookAndFeel::KnobLookAndFeel(const char* knobData, const int knobDataSize, const int knobSize)
+{
+    knobImage = std::make_unique<KnobImage>(knobData, knobDataSize, knobSize);
+}
+
+KnobLookAndFeel::~KnobLookAndFeel()
+{}
+
+void KnobLookAndFeel::setKnobImage(const char* knobData, const int knobDataSize, const int knobSize) {
+    knobImage = std::make_unique<KnobImage>(knobData, knobDataSize, knobSize);
+}
+
+juce::Font KnobLookAndFeel::getBaseFont()
+{
+    return juce::Font::bold;
+}
+
+juce::Font KnobLookAndFeel::getLabelFont(juce::Label&)
+{
+    return getBaseFont().withPointHeight(10);
+}
+
+juce::Font KnobLookAndFeel::getSliderReadoutFont()
+{
+    return getBaseFont().withPointHeight(14);
+}
+
+void KnobLookAndFeel::drawLabel(juce::Graphics& g, juce::Label& l)
+{
+    juce::Colour labelColour = juce::Colour::fromRGB(149, 89, 17);
+    juce::Font labelFont = getLabelFont(l);
+
+    g.setColour(labelColour);
+    g.setFont(labelFont);
+
+    juce::Rectangle<int> textArea(l.getBorderSize().subtractedFrom(l.getLocalBounds()));
+
+    g.drawFittedText(l.getText(), textArea, l.getJustificationType(),
+        juce::jmax(1, (int)(textArea.getHeight() / labelFont.getHeight())),
+        l.getMinimumHorizontalScale());
+}
+
+void KnobLookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPos,
+    const float rotaryStartAngle, const float rotaryEndAngle, juce::Slider& slider)
+{
+    const float radius = juce::jmin(width / 2, height / 2) - 2.0f;
+    const float centreX = x + width * 0.5f;
+    const float centreY = y + height * 0.5f;
+    const float rx = centreX - radius;
+    const float ry = centreY - radius;
+    const float rw = radius * 2.0f;
+    const float angle = rotaryStartAngle + sliderPos * (rotaryEndAngle - rotaryStartAngle);
+    const bool isMouseOver = slider.isMouseOverOrDragging() && slider.isEnabled();
+
+    // Draw the readout
+    // Change this to hint box?
+    juce::AffineTransform transform = juce::AffineTransform();
+    
+    g.drawImageTransformed(knobImage->getImage(), transform.rotation(angle, knobImage->pivotPoint, knobImage->pivotPoint));
+
+    juce::Colour readoutColour = juce::Colour::fromRGB(254, 173, 29).withAlpha(isMouseOver ? 1.0f : 0.9f);
+    const double value = slider.getValue();
+    juce::String readoutValue = (value >= 1000.0 ? juce::String(value / 1000.0, 1) + "k" : juce::String(value, 1));
+    juce::String readout = readoutValue;
+
+    g.setColour(readoutColour);
+    g.setFont(getSliderReadoutFont());
+    g.drawText(readout, centreX - radius, centreY - 10.0f, rw, 24.0f, juce::Justification::centred);
+
+    // Draw the track
+    g.setColour(slider.findColour(juce::Slider::rotarySliderOutlineColourId));
+    /*
+    juce::Path track;
+    track.addArc(rx, ry, rw, rw, rotaryStartAngle, rotaryEndAngle, true);
+    g.strokePath(track, juce::PathStrokeType(3.0f));
+
+    // Draw the slider position
+    juce::Colour sliderFillStart = juce::Colour::fromRGBA(245, 121, 35, 255).withAlpha(isMouseOver ? 1.0f : 0.9f);
+    juce::Colour sliderFillStop = juce::Colour::fromRGBA(255, 184, 23, 255).withAlpha(isMouseOver ? 1.0f : 0.9f);
+    juce::ColourGradient sliderFill = juce::ColourGradient(sliderFillStart,
+        (float)x,
+        (float)0,
+        sliderFillStop,
+        (float)width,
+        (float)0,
+        false);
+
+    g.setGradientFill(sliderFill);
+
+    juce::Path filledArc;
+    filledArc.addArc(rx, ry, rw, rw, rotaryStartAngle, angle, true);
+    juce::PathStrokeType(3.0f).createStrokedPath(filledArc, filledArc);
+    g.fillPath(filledArc);*/
+}
---
+++ Source/UI/Knobs/KnobLookAndFeel.h
@@ -0,0 +1,40 @@
+/*
+  ==============================================================================
+
+    FaugLookAndFeel.h
+    Created: 8 Aug 2022 11:55:23am
+    Author:  tytu1
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+class KnobImage;
+
+//==============================================================================
+/*
+*/
+class KnobLookAndFeel  : public juce::LookAndFeel_V4
+{
+    public:
+        KnobLookAndFeel();
+        KnobLookAndFeel(const char* knobData, const int knobDataSize, const int knobSize);
+        virtual ~KnobLookAndFeel() override;
+
+        void setKnobImage(const char* knobData, const int knobDataSize, const int knobSize);
+
+        juce::Font getBaseFont();
+        juce::Font getLabelFont(juce::Label&) override;
+        juce::Font getSliderReadoutFont();
+
+        void drawLabel(juce::Graphics&,juce::Label&) override;
+        void drawRotarySlider(juce::Graphics&, int x, int y, int width, int height,
+            float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle,
+            juce::Slider&) override;
+
+    private:
+        std::unique_ptr<KnobImage> knobImage;
+};
